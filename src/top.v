f09056ae17bfa42d3f0c5143febc82009dc96333
Robbel121 <135472469+Robbel121@users.noreply.github.com>
2025-11-05 19:14:15 -0700

    Update top.v

--- src/top.v
+++ src/top.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/11/2025 03:48:34 PM
-// Design Name: 
-// Module Name: top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module top(
     input clk,
@@ -30,7 +10,7 @@ module top(
     wire tenhzclk;
     wire [7:0] w1s, w10s, w100s, w1000s;
     
-    OneHzEnable hzten(.clk(clk), .rst(rst), .tenHzClk(tenhzclk));
+    TenHzEnable hzten(.clk(clk), .rst(rst), .tenHzClk(tenhzclk));
     
     digits dig(.tenHzClk(tenhzclk), .rst(rst), .ones(w1s), .tens(w10s), .hundreds(w100s),.thousands(w1000s));
     
