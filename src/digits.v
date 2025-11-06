2a7021a2f0a5a5c50cdfd14a5c2d6b7849c6c150
Robbel121 <135472469+Robbel121@users.noreply.github.com>
2025-11-05 19:14:44 -0700

    Update digits.v

--- src/digits.v
+++ src/digits.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/11/2025 04:12:23 PM
-// Design Name: 
-// Module Name: digits
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module digits(
     input tenHzClk,
