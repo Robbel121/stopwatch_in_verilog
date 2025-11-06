e2fa2fa7f5432d69ef2bd5f7a6598dcff240e71b
Robbel121 <135472469+Robbel121@users.noreply.github.com>
2025-11-05 19:14:34 -0700

    Update sevenSegControl.v

--- src/sevenSegControl.v
+++ src/sevenSegControl.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/11/2025 04:30:18 PM
-// Design Name: 
-// Module Name: sevenSegControl
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module sevenSegControl(
     input clk,
